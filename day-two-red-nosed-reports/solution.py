d40afa8ccc9d3e97e231890ef391b1d357d732f2
Syed Muhammad Shiraz Jafri <syja00001@stud.uni-saarland.de>
2024-12-02 17:08:17 +0100

    simplify boolean conditions and reduce code

--- day-two-red-nosed-reports/solution.py
+++ day-two-red-nosed-reports/solution.py
@@ -5,23 +5,17 @@ def safe(report_from_file):
     all_positive = all(diff > 0 for diff in differences)
     all_negative = all(diff < 0 for diff in differences)
     diff_within_3 = all(1 <= abs(diff) <= 3 for diff in differences)
-    if (all_positive and diff_within_3) or (all_negative and diff_within_3):
-        return True
-    else:
-        return False
+
+    return (all_positive or all_negative) and diff_within_3
 
 
 def diffs(list_of_values):
-    diffs_to_return = []
-    for i in range(1, len(list_of_values)):
-        diffs_to_return.append(list_of_values[i] - list_of_values[i - 1])
-    return diffs_to_return
+    return [list_of_values[i] - list_of_values[i - 1] for i in range(1, len(list_of_values))]
 
 no_of_safe_reports = 0
 with open('input.txt') as reports:
     for report in reports:
         report = list(map(int, report.split()))
-        if safe(report):
-            no_of_safe_reports += 1
+        no_of_safe_reports += 1 if safe(report) else 0
 
 print(f'Total number of safe reports: {no_of_safe_reports}.')
